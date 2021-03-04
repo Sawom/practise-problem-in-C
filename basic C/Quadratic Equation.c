59079f9aa5f98081491175ee8f8318f7960cf300
Sawom <64780532+Sawom@users.noreply.github.com>
2021-03-04 15:51:30 +0600

    Update Quadratic Equation.c

--- basic C/Quadratic Equation.c
+++ basic C/Quadratic Equation.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 #include <math.h>
-void main(){
+int main(){
    int a,b,c,d;
    float x1,x2;
 
    printf("Input the value of a,b & c : ");
    scanf("%d%d%d",&a,&b,&c);
    d=b*b-4*a*c;
-   if(d==0)
-   {
+   if(d==0){
      printf("Both roots are equal.\n");
      x1=-b/(2.0*a);
      x2=x1;
      printf("First  Root Root1= %f\n",x1);
      printf("Second Root Root2= %f\n",x2);
    }
-   else if(d>0)
-	{
+   else if(d>0){
 	   printf("Both roots are real and diff-2\n");
 	   x1=(-b+sqrt(d))/(2*a);
 	   x2=(-b-sqrt(d))/(2*a);
